07d76027d197c39e5baab1556458ea4b06d9cfee
Dwi Candra Kirana <dwicandra920@gmail.com>
2023-11-22 22:56:25 +0700

    remove code and file unused

--- app/src/main/java/com/vt/sentuhdigitalteknologitest/core/api/ApiService.java
+++ app/src/main/java/com/vt/sentuhdigitalteknologitest/core/api/ApiService.java
@@ -1,7 +1,6 @@
 package com.vt.sentuhdigitalteknologitest.core.api;
 
 import com.vt.sentuhdigitalteknologitest.core.model.SearchDummyResponse;
-import com.vt.sentuhdigitalteknologitest.core.model.SearchResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
